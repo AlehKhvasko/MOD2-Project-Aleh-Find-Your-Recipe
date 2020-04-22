23540b628b3d6edcaa5791543b7e3765fe4ffb36
Aleh <user@imac-user.myfiosgateway.com>
2020-04-21 21:36:34 -0400

    added new goals to reach over the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 
+//register on api.edamam.com and get valid recipe API
+//install and use AXIOS
+//create get request with AXIOS, use ID and KEY separate 
+//store data in state
+//create search form and use it as a component
+
+//In plans:
+//Implement semantic UI
+//Try to use Redux
+//Use Router to switch between pages
+
 function App() {
 const APP_ID = 'a8e02ed5';
 const APP_KEY='16dd19f01ad1a5fc17a8c9206134115b';
@@ -18,7 +29,7 @@ const recipeData = async() => {
   
 return (
     <div className="App">
-      <h1 onClick={recipeData} >React food app</h1>
+      <h1 onClick={recipeData} >My Grandma's recipe</h1>
     </div>
   );
 }
